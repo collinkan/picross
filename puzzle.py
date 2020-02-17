19470296c3435791e06059fc227df817a47ed563
Collin Kan <kanc@mcmaster.ca>
2020-02-17 07:37:00 -0500

    Added state variable to constructor

--- puzzle.py
+++ puzzle.py
@@ -3,6 +3,11 @@
 class puzzle:
 
     def __init__(self, solution: list, row_hints: list, col_hints: list):
+        self.grid = [[0 for n in solution[0]] for r in solution]
         self.solution = solution
         self.row_hints = row_hints
         self.col_hints = col_hints
+
+    def add(self) -> None:
+        return
+    
