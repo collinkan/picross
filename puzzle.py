1b96dfc5eadfe98eba862b8c85caf6f7552920c8
Collin Kan <kanc@mcmaster.ca>
2020-02-17 07:23:35 -0500

    Added puzzle class and constructor

--- puzzle.py
+++ puzzle.py
@@ -1,5 +1,8 @@
 #Collin Kan
 
-class grid:
+class puzzle:
 
-    def __init__(self, )
+    def __init__(self, solution: list, row_hints: list, col_hints: list):
+        self.solution = solution
+        self.row_hints = row_hints
+        self.col_hints = col_hints
