784b5b3d17fe6ed11a5f4349f2bf14d8cdd4fe0e
Collin Kan <kanc@mcmaster.ca>
2020-02-17 08:07:31 -0500

    Created display() method

--- picross_game.py
+++ picross_game.py
@@ -1,1 +1,13 @@
 #Collin Kan
+
+from tkinter import *
+from puzzle import *
+
+W_WIDTH = 1000
+W_HEIGHT = 1000
+
+def game_display(pzl : puzzle) -> None:
+    gamew = Tk()
+    gamew.geometry('1000x1000')
+    gamew.title("Collin's Picross")
+
