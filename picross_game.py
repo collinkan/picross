268c67e8d3631abfe276915eeaaa91a244e1cf1a
Collin Kan <kanc@mcmaster.ca>
2020-02-18 05:59:02 -0500

    added buttons

--- picross_game.py
+++ picross_game.py
@@ -5,14 +5,14 @@ from puzzle import *
 
 W_WIDTH = 1000
 W_HEIGHT = 1000
-pzl = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
+pzl = [[1,2], [3, 4]]
 
 def game_display() -> None:
     gamew = Tk()
     gamew.title("Collin's Picross")
-    boxes = [[Button(gamew, command=clicked(toggle()), height=7, width=15) for n in pzl[0]] for r in pzl]
+    boxes = [[Button(gamew, command=clicked(toggle()), height=7, width=15) for r in pzl[0]] for n in pzl]
     for i in range(len(boxes)):
-        for j in range(len(boxes[i])):
+        for j in range(len(boxes[0])):
             boxes[i][j].grid(column=i, row=j)
     gamew.mainloop()
 
