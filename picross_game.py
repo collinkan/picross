575ddf9192df99dca02bc856db024805156d1ba4
Collin Kan <kanc@mcmaster.ca>
2020-02-19 06:16:21 -0500

    created PicrossGame class and moved functions into class

--- picross_game.py
+++ picross_game.py
@@ -16,17 +16,22 @@ rhints = [[1],
 chints = [[],[1],[5],[1],[]]
 pzl = puzzle(solution,rhints,chints)
 
-def game_loop(pzl: puzzle) -> None:
-    print("?")
-    while(pzl.solved() == False):
-        print(np.matrix(pzl.grid))
-        user_input(pzl)
+class PicrossGame:
 
-def user_input(pzl: puzzle) -> None:
-    move = [ int(x) for x in input("Row and Column (x,y): ").split(",") ]
-    if move[0] in range(1,6) and move[1] in range(1,6):
-        pzl.select(move[0] - 1, move[1] - 1)
-    else: print("Invalid square, try again.\n")
+    def __init__(self, pzl: puzzle):
+        self.pzl = pzl
+
+    def game_loop(self) -> None:
+        print("?")
+        while(pzl.solved() == False):
+            print(np.matrix(pzl.grid))
+            user_input(pzl)
+
+    def user_input(self) -> None:
+        move = [ int(x) for x in input("Row and Column (x,y): ").split(",") ]
+        if move[0] in range(1,6) and move[1] in range(1,6):
+            pzl.select(move[0] - 1, move[1] - 1)
+        else: print("Invalid square, try again.\n")
 
 
 game_loop(pzl)
