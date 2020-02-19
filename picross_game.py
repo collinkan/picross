b0a1cc25a2ee122f0bb9f51070e754fef267f74b
Collin Kan <kanc@mcmaster.ca>
2020-02-19 07:30:09 -0500

    added mode select to user_input()

--- picross_game.py
+++ picross_game.py
@@ -13,15 +13,18 @@ class PicrossGame:
 
     def game_loop(self) -> None:
         while(self.get_pzl().solved() == False):
-            print(np.c_[self.get_pzl().get_row_hints(), self.get_pzl().get_grid()])
+            print(np.matrix(self.get_pzl().get_grid()))
             self.user_input()
         print("You solved the puzzle!")
 
     def user_input(self) -> None:
         move = [ int(x) for x in input("Row and Column (x,y): ").split(",") ]
+        mode = input("Select (s) or Mark (m) :")
         if (move[0] in range(1, len(self.get_pzl().get_grid())+1) 
         and move[1] in range(1, len(self.get_pzl().get_grid())+1)):
-            self.get_pzl().select(move[0] - 1, move[1] - 1)
+            if mode == 'm': self.get_pzl().mark(move[0] - 1, move[1] - 1)
+            elif mode == 's': self.get_pzl().select(move[0] - 1, move[1] - 1)
+            else: print("Invalid mode, try again.\n") 
         else: print("Invalid square, try again.\n")
 
 
