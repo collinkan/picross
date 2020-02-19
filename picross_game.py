1d5d35cd8326768b01c3330f56f2b20ede2e1e9a
Collin Kan <kanc@mcmaster.ca>
2020-02-19 07:12:16 -0500

    rearranged order

--- picross_game.py
+++ picross_game.py
@@ -3,19 +3,6 @@
 from puzzle import *
 import numpy as np
 
-solution = [[0,0,1,0,0],
-            [0,1,1,1,0],
-            [0,0,1,0,0],
-            [0,0,1,0,0],
-            [0,0,1,0,0]]
-rhints = [[1],
-          [3],
-          [1],
-          [1],
-          [1]]
-chints = [[],[1],[5],[1],[]]
-pzl = puzzle(solution,rhints,chints)
-
 class PicrossGame:
 
     def __init__(self, pzl: puzzle):
@@ -26,16 +13,30 @@ class PicrossGame:
 
     def game_loop(self) -> None:
         while(self.get_pzl().solved() == False):
-            print(np.matrix(self.get_pzl().get_grid()))
+            print(np.c_[self.get_pzl().get_row_hints(), self.get_pzl().get_grid()])
             self.user_input()
         print("You solved the puzzle!")
 
     def user_input(self) -> None:
         move = [ int(x) for x in input("Row and Column (x,y): ").split(",") ]
-        if move[0] in range(1, len(self.get_pzl().get_grid())+1) and move[1] in range(1, len(self.get_pzl().get_grid())+1):
+        if (move[0] in range(1, len(self.get_pzl().get_grid())+1) 
+        and move[1] in range(1, len(self.get_pzl().get_grid())+1)):
             self.get_pzl().select(move[0] - 1, move[1] - 1)
         else: print("Invalid square, try again.\n")
 
+
+solution = [[0,0,1,0,0],
+            [0,1,1,1,0],
+            [0,0,1,0,0],
+            [0,0,1,0,0],
+            [0,0,1,0,0]]
+rhints = [[1],
+          [3],
+          [1],
+          [1],
+          [1]]
+chints = [[0],[1],[5],[1],[0]]
+pzl = puzzle(solution,rhints,chints)
 
 pg = PicrossGame(pzl)
 pg.game_loop()
