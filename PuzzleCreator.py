f60de9e2c9f7d171318c70d16873cac66df50a9b
Collin Kan <kanc@mcmaster.ca>
2020-02-20 06:29:48 -0500

    changed spacing

--- PuzzleCreator.py
+++ PuzzleCreator.py
@@ -6,16 +6,14 @@ import imageio
 def solution_maker(imgname: str) -> list:
     img = imageio.imread("PuzzleImages\\"+imgname).tolist()
     solution = [[] for pixel in img]
-
     for i in range(len(img)):
         for j in range(len(img)):
             solution[i].append(int(img[i][j][0] == 0))
-
     return solution
+
 
 def row_hints_maker(solution: list) -> list:
     row_hints = [[] for r in solution]
-    
     for i in range(len(solution)):
         j = 0
         while(j < len(solution[i])):
@@ -26,12 +24,11 @@ def row_hints_maker(solution: list) -> list:
                     j += 1 
                 row_hints[i].append(counter)
             j +=1
-
     return row_hints
+
 
 def col_hints_maker(solution: list) -> list:
     col_hints = [[] for n in solution[0]]
-
     for j in range(len(solution[0])):
         i = 0
         while(i < len(solution)):
@@ -42,5 +39,4 @@ def col_hints_maker(solution: list) -> list:
                     i += 1 
                 col_hints[j].append(counter)
             i +=1
-
     return col_hints
