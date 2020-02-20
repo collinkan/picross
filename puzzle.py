2be70845a450f77953e3433f56386fb2303a55e3
Collin Kan <kanc@mcmaster.ca>
2020-02-20 06:12:30 -0500

    added getter method for new state variable and created new state variable grid_copy

--- puzzle.py
+++ puzzle.py
@@ -8,14 +8,21 @@ class Puzzle:
         self.imgname = imgname
         self.solution = pc.solution_maker(self.imgname)
         self.grid = [[0 for n in self.solution[0]] for r in self.solution]
+        self.grid_copy = self.grid
         self.row_hints = pc.row_hints_maker(self.solution)
         self.col_hints = pc.col_hints_maker(self.solution)
+
+    def get_imgname(self) -> str:
+        return self.imgname
+
+    def get_solution(self) -> list:
+        return self.solution
 
     def get_grid(self) -> list:
         return self.grid
 
-    def get_solution(self) -> list:
-        return self.solution
+    def get_grid_copy(self) -> list:
+        return self.grid_copy
 
     def get_row_hints(self) -> list:
         return self.row_hints
@@ -24,8 +31,12 @@ class Puzzle:
         return self.col_hints
         
     def select(self, x: int, y: int) -> None:
-        if self.get_grid()[x][y] == 0: self.grid[x][y] = 1
-        elif self.get_grid()[x][y] == 1: self.grid[x][y] = 0
+        if self.get_grid()[x][y] == 0: 
+            self.grid[x][y] = 1
+            self.grid_copy[x][y] = 1
+        elif self.get_grid()[x][y] == 1: 
+            self.grid[x][y] = 0
+            self.grid_copy[x][y] = 0
 
     def mark(self, x: int, y:int) -> None:
         if self.get_grid()[x][y] == 0: self.grid[x][y] = 2
@@ -34,7 +45,7 @@ class Puzzle:
     def solved(self) -> bool:
         for i in range(len(self.get_grid())):
             for j in range(len(self.get_grid()[i])):
-                if self.get_grid()[i][j] != self.get_solution()[i][j]:
+                if self.get_grid_copy()[i][j] != self.get_solution()[i][j]:
                     return False
         return True
 
