afa0f69da1c7148f9713ba9fc7addbc0967ff595
Collin Kan <kanc@mcmaster.ca>
2020-02-20 06:27:53 -0500

    edited grid_copy

--- puzzle.py
+++ puzzle.py
@@ -8,7 +8,7 @@ class Puzzle:
         self.imgname = imgname
         self.solution = pc.solution_maker(self.imgname)
         self.grid = [[0 for n in self.solution[0]] for r in self.solution]
-        self.grid_copy = self.grid
+        self.grid_copy = [[0 for n in self.solution[0]] for r in self.solution]
         self.row_hints = pc.row_hints_maker(self.solution)
         self.col_hints = pc.col_hints_maker(self.solution)
 
@@ -43,8 +43,8 @@ class Puzzle:
         elif self.get_grid()[x][y] == 2: self.grid[x][y] = 0
 
     def solved(self) -> bool:
-        for i in range(len(self.get_grid())):
-            for j in range(len(self.get_grid()[i])):
+        for i in range(len(self.get_grid_copy())):
+            for j in range(len(self.get_grid_copy()[i])):
                 if self.get_grid_copy()[i][j] != self.get_solution()[i][j]:
                     return False
         return True
