9bd88404568c27246b4d35e0f10c63af98d25420
Collin Kan <kanc@mcmaster.ca>
2020-02-25 03:24:59 -0500

    Created constructor

--- Button.py
+++ Button.py
@@ -1,1 +1,12 @@
 #Collin Kan
+
+import pygame
+
+class Button:
+
+    def __init__(self, x: int, y: int, width: int, height: int, text: str):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
