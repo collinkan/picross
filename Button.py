854b0efeee5c88066f736a9dd743eba640b57c8e
Collin Kan <kanc@mcmaster.ca>
2020-02-25 03:44:01 -0500

    Created draw and isHover methods

--- Button.py
+++ Button.py
@@ -10,3 +10,16 @@ class Button:
         self.width = width
         self.height = height
         self.text = text
+
+    def draw(self, win: pygame.display) -> None:
+        pygame.draw.rect(win, (0,0,0), (self.x, self.y, self.width, self.height), 0)
+        pygame.draw.rect(win, (255,255,255), (self.x+2, self.y+2, self.width-4, self.height-4))
+        font = pygame.font.SysFont('arial', 60)
+        text = font.render(self.text, 1, (0,0,0))
+        win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def isHover(self, pos):
+        if (pos[0] > self.x and pos[0] < self.x + self.width
+        and pos[1] > self.y and pos[1] < self.y + self.height):
+            return True
+        return False
