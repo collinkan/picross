387836799d5a9cf4c6dac4535f9eba3ebb725b3f
Collin Kan <kanc@mcmaster.ca>
2020-02-25 04:47:50 -0500

    Added buttons state variable to constructor and corresponding getter method

--- PicrossGUI.py
+++ PicrossGUI.py
@@ -4,15 +4,34 @@ import pygame
 from Button import *
 from PicrossGame import *
 
-class PicrossGUI(PicrossGame):
+class PicrossGUI:
 
-    def __init__(self, pzl: Puzzle, win: pygame.display):
-        super.__init__(self, pzl)
-        self.win = win
+    def __init__(self, pzl: Puzzle):
+        self.pzl = pzl
+        self.win = pygame.display.set_mode((800,900))
+        self.buttons = [[] for r in self.get_pzl().get_grid()]
+
+        dim = 500 / len(self.get_pzl().get_grid())
+        for i in range(len(self.get_pzl().get_grid())):
+            for j in range(len(self.get_pzl().get_grid()[0])):
+                x = 300 + (dim * i)
+                y = 300 + (dim * i)
+                square = Button(x,y,dim,dim)
+                self.buttons[i].append(square)
+
 
     def get_pzl(self) -> Puzzle:
-        super.get_pzl(self)
+        return self.pzl
 
     def get_win(self) -> pygame.display:
         return self.win
-    
+
+    def get_buttons(self) -> list:
+        return self.buttons
+
+    def drawGrid(self) -> None:
+        for r in self.get_buttons():
+            for b in r:
+                b.draw(self.win)
+
+                
