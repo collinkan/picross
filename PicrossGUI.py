76c5ec2b4c9223aaf25926865cd667219ba1856b
Collin Kan <kanc@mcmaster.ca>
2020-02-25 04:29:32 -0500

    Created get_pzl and get_win

--- PicrossGUI.py
+++ PicrossGUI.py
@@ -9,3 +9,10 @@ class PicrossGUI(PicrossGame):
     def __init__(self, pzl: Puzzle, win: pygame.display):
         super.__init__(self, pzl)
         self.win = win
+
+    def get_pzl(self) -> Puzzle:
+        super.get_pzl(self)
+
+    def get_win(self) -> pygame.display:
+        return self.win
+    
