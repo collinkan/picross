3796e11acca5861ed2ebafb26b9a0691814dd4cd
Collin Kan <kanc@mcmaster.ca>
2020-02-25 18:34:18 -0500

    Changed some values

--- Button.py
+++ Button.py
@@ -18,9 +18,10 @@ class Button:
     def draw(self, win: pygame.display) -> None:
         pygame.draw.rect(win, self.outline, (self.x, self.y, self.width, self.height), 0)
         pygame.draw.rect(win, self.color, (self.x+2, self.y+2, self.width-4, self.height-4))
-        font = pygame.font.Font('freesansbold.ttf', 60)
-        text = font.render(self.text, 1, (0,0,0))
-        win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+        if self.text != "":
+            font = pygame.font.Font('freesansbold.ttf', 40)
+            text = font.render(self.text, 1, self.outline)
+            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
     def toggle(self) -> None:
         if self.color == (0,0,0): self.color = (255,255,255)
